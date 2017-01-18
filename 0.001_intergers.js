def4f79fcd9014f8dbab6498b249b9ee7b20d23c
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-17 19:00:14 -0500

    initial commit

---
+++ 0.001_intergers.js
@@ -0,0 +1,15 @@
+console.log(7.6);
+console.log(8+2);
+console.log(700/54);
+console.log(7*53208549);
+console.log(700%54);
+console.log(1.2%7.92);
+console.log(17%5);
+console.log(27%5);
+console.log(50%7);
+console.log(51%7);
+console.log(51%2);
+console.log(51%3);
+console.log(103%12);
+console.log(Math.pow(2, 7));
+console.log(Math.pow(10, 21));
