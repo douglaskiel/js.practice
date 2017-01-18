bb9a93be52179d513b1dff3c6bfda5b31601bab0
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-17 19:59:46 -0500

    added variables

---
+++ 0.002_variables.js
@@ -0,0 +1,46 @@
+var girlfriend = "Abby";
+var desire = "Marry";
+var glastname = "Hartman";
+var fullname = girlfriend +" "+ glastname
+
+console.log("I want to " + desire + " " + fullname);
+
+console.log(num);
+var num;
+
+var abbysAge = 34;
+
+// console.log(abbysAge -= 4);
+// console.log(abbysAge += 7);
+// console.log(abbysAge /= 7);
+// console.log(abbysAge *= 6);
+// console.log(abbysAge -= 7);
+// console.log(abbysAge /= 6);
+// console.log(abbysAge += 4);
+// console.log(abbysAge *= 7);
+// console.log(abbysAge /= 7);
+// console.log(abbysAge -= 4);
+// console.log(abbysAge *= 6);
+// console.log(abbysAge += 7);
+// console.log(abbysAge /= 6);
+// console.log(abbysAge *= 7);
+// console.log(abbysAge -= 7);
+// console.log(abbysAge -= 0.000000000000014);
+// console.log(abbysAge += 4);
+
+
+var schoolname = "Eleven Fifty";
+var schooltype = "Coding School";
+var schoollength = "Two Years";
+var schoollocal = "Launch Fishers";
+var schoololdlocal = "1150 116th St., Carmel";
+
+console.log(schoolname +" "+ "is a " + schooltype + " that has been around for " + schoollength + " starting at " + schoololdlocal + " and is now at " + schoollocal + ".");
+
+
+
+
+
+
+
+
