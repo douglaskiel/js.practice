b8cb1a15debeb42364c8af5bd6a167a11593100e
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-18 19:00:21 -0500

    added vanilla string practice

---
+++ 0.003_strings.js
@@ -0,0 +1,9 @@
+var myName = "Douglas Kiel";
+var message = myName + ": " + 41;
+var age = 41;
+var herName = "Abby Hartman";
+var herAge = 34;
+var hmessage = herName + ": " + 34;
+
+console.log(herName + ": " + 34);
+console.log(myName + ": " + 41);
