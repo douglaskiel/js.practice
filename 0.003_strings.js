72ab5348d8413ab09cdb46c11f5af05c3d2a4c12
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-19 08:51:16 -0500

    added challenges

--- 0.003_strings.js
+++ 0.003_strings.js
@@ -7,3 +7,28 @@ var hmessage = herName + ": " + 34;
 
 console.log(herName + ": " + 34);
 console.log(myName + ": " + 41);
+// console.log(herName +" is seven years and eight months younger than "+ myName +" because "+ myName +" is "+ age +" and "+ herName +" is "+ herage += 1 + " in February.");
+console.log(herName.length);
+console.log(myName.length);
+
+// Bronze: 
+var mySchool = "Ball State";
+var grad = "2002";
+
+console.log("I left " + mySchool + " in " + grad + '.');
+
+// Silver:
+var make = "MVP";
+var model = "Mazda";
+var year = 2005;
+
+var car = year +" "+ model +" "+ make;
+
+console.log("I own a " + car +".");
+
+// Gold:
+
+
+
+
+
