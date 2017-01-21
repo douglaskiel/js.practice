beb889c415b83feae6e9898bc7d70c5090cdd1a7
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-21 12:35:00 -0500

    added if/conditionals

---
+++ 0.005_if.js
@@ -0,0 +1,29 @@
+var myNum = 10;
+
+if (myNum > 9) {
+	console.log("myNum is greater than 10");
+}
+
+var loud = true;
+var soft = false;
+
+if (loud === true) {
+	console.log("Your music is too loud")
+}
+
+if (loud && soft) {
+	console.log("Would you like to turn that down");
+}
+
+if (loud || soft) {
+	console.log("Korean music is not as good as other music.");
+}
+
+var beautiful = true;
+var unattractive = false;
+
+if (beautiful || unattractive) {
+	console.log("Abby is beautiful!");
+} else {
+	console.log("Abby is ugly!")
+}
