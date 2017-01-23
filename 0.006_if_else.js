ddedf2ec7dd74181d806be42cd8ee6380a374f20
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-23 18:33:46 -0500

    added if else

---
+++ 0.006_if_else.js
@@ -0,0 +1,21 @@
+var abbyGirlfriend = true;
+var abbyLikesTrump = false;
+
+if (abbyGirlfriend == true) {
+	console.log("Abby is beautiful!");
+} else {
+	console.log("Trump is the best President we'll have.");
+}
+
+if (abbyLikesTrump === false) {
+	console.log("Donald Trump should watch out!");
+} else {
+	console.log("Donald Trump will help us all.");
+}
+
+var num1 = 1
+var str1 = "1"
+
+var bankAccount = 
+var debt = 
+
