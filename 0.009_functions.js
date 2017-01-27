16affd77a88d336a6f83d8f0322c4e9e4a367d40
seeker.of.light <seekeroflight75@yahoo.com>
2017-01-27 14:22:23 -0500

    relearning functions

---
+++ 0.009_functions.js
@@ -0,0 +1,12 @@
+function sumNum(num2, num3) {
+
+	console.log(num2 + num3);
+}
+
+sumNum(10, 28);
+
+function complete(course) {
+	return "Douglas " + "has his" + course + ".";
+}
+
+complete("gold badge");
